9c6f4de3e13872975cee8ae3c92826f38962a8d9
NicoVedia <nicovedia04@gmail,com>
2023-08-20 22:41:03 -0300

    mercado-liebre

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, ()=>{
+    console.log('Servidor funcionando ${PORT}');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
